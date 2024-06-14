167bc883a6c0b712e1e1e509a60bc0ed684d84e0
Wiola-D <wioletta.d.work@gmail.com>
2024-06-14 12:34:37 +0200

    Remove node_modules

---
+++ contacts.js
@@ -0,0 +1,21 @@
+const fs = require("fs").promises;
+const path = require("path");
+
+const contactsPath = path.join(__filename);
+console.log(contactsPath);
+// TODO: udokumentuj każdą funkcję
+function listContacts() {
+  // ...twój kod
+}
+
+function getContactById(contactId) {
+  // ...twój kod
+}
+
+function removeContact(contactId) {
+  // ...twój kod
+}
+
+function addContact(name, email, phone) {
+  // ...twój kod
+}
